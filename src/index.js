f67ec84d2bba8b366a467e93772e92e0c8d93d5c
GaciuRadu <gaciuradu@gmail.com>
2023-07-05 18:24:14 +0300

    idem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import "./style.css";
 
-console.warn("start app");
+// console.warn("start app");
+
+function getTeamAsHTML(team) {
+  // console.info(team);
+  return `<tr>
+    <td></td>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>    
+    <td>x</td>
+    </tr>`;
+}
+
+function renderTeams(teams) {
+  //   console.warn("render", teams);
+  const htmlTeams = teams.map(getTeamAsHTML);
+  //   console.warn(htmlTeams);
+  document.querySelector("#teamsTable tbody").innerHTML = htmlTeams.join("");
+}
+
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
