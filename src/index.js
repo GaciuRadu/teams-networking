2b10514dd6297d4d6dd329e2bbe7fd134f627ac3
GaciuRadu <gaciuradu@gmail.com>
2023-07-05 21:24:34 +0300

    create function createTeamRequest/function onSubmit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function createTeamRequest(team) {
+  return fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  }).then(r => r.json());
+}
+
 // console.warn("start app");
 
 function getTeamAsHTML(team) {
@@ -10,7 +24,7 @@ function getTeamAsHTML(team) {
     <td>${team.members}</td>
     <td>${team.name}</td>
     <td>${team.url}</td>    
-    <td>x</td>
+    <td>x 🖍️</td>
     </tr>`;
 }
 
@@ -18,7 +32,7 @@ function renderTeams(teams) {
   //   console.warn("render", teams);
   const htmlTeams = teams.map(getTeamAsHTML);
   //   console.warn(htmlTeams);
-  document.querySelector("#teamsTable tbody").innerHTML = htmlTeams.join("");
+  $("#teamsTable tbody").innerHTML = htmlTeams.join("");
 }
 
 function loadTeams() {
@@ -27,4 +41,32 @@ function loadTeams() {
     .then(renderTeams);
 }
 
+function onSubmit(e) {
+  //   console.warn("submit", e);
+  e.preventDefault();
+
+  const members = $("#members").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
+  const team = {
+    promotion: $("#promotion").value,
+    members: members,
+    name,
+    url
+  };
+  console.warn(team);
+
+  createTeamRequest(team).then(status => {
+    console.warn("created", status);
+    if (status.success) {
+      window.location.reload();
+    }
+  });
+}
+
+function initEvents() {
+  $("#teamsForm").addEventListener("submit", onSubmit);
+}
+
 loadTeams();
+initEvents();
