409bf4271203f116a2057184f2c57398c4de287a
GaciuRadu <gaciuradu@gmail.com>
2023-07-10 15:16:53 +0300

    new file for teste

---
+++ src/teste.js
@@ -0,0 +1,30 @@
+// Teste
+
+// console.info(typeof document.getElementsByTagName("body")[0]);
+// console.info(typeof {});
+// console.info(typeof []);
+
+// var string = "String";
+// if ("string" != string.trim()) console.info("case1");
+// // else console.info("case2");
+
+// var x;
+// console.warn(x);
+
+// function name() {
+//   if ("yes" == "yes") {
+//     console.warn("yes");
+//   }
+// }
+// name();
+
+// var firstName = "Radu";
+// var middleName = "Cristian";
+// var lastName = "Gaciu";
+// var fullName = `${firstName} ${middleName} ${lastName}`;
+// var fullName = "firstName + LastName";
+// console.warn(fullName);
+
+// for (var i = 0; i < 5; i++) {
+//   console.info(i);
+// }
